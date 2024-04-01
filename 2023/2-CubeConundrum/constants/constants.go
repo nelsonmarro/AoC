0c599218406a1eaabc98840466ee6ce96daf589f
Nelson Marro <nelsonmarro99@gmail.com>
2024-03-31 22:04:50 -0500

    challenge completed

---
+++ 2023/2-CubeConundrum/constants/constants.go
@@ -0,0 +1,4 @@
+package constants
+
+const FinalInputPath string = "/data/final-path.txt"
+const TestInputPath string = "/data/test-path.txt"
---
+++ 2023/2-CubeConundrum/go.mod
@@ -0,0 +1,3 @@
+module github.com/aoc/2023/2
+
+go 1.22.1
